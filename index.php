8484134b26c3b75dec4ba2d4d9942d86f3476a47
ruby <ruby.van.soelen@omines.com>
2022-12-12 18:00:42 +0100

    Refactoring: optimized functionality

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $jsonTestCases = file_get_contents('testcases.json');
 $testCases = json_decode($jsonTestCases);
-foreach($testCases as $caseName => $caseData) {
-    echo "-----------------------------------<br />";
+foreach ($testCases as $caseName => $caseData) {
+    echo '-----------------------------------<br />';
     echo $caseName . ' wordt getest<br />';
     echo 'expected output: <br />';
     echo str_replace("\n", '<br />', str_replace(' ', '&nbsp;', $caseData->expectedOutput)) . '<br /><br />';
@@ -19,33 +19,33 @@ foreach($testCases as $caseName => $caseData) {
     extractOutputFromInput($inputArray);
 }
 
-function extractOutputFromInput($n)
+function extractOutputFromInput(array $n): void
 {
     $arrays = [];
     $valid = 'true';
 
-    $rowNumber = 0;
+    // add squares
     foreach ([[0, 1, 2], [3, 4, 5], [6, 7, 8]] as $xGroup) {
         foreach ([[0, 1, 2], [3, 4, 5], [6, 7, 8]] as $yGroup) {
-            $arrays[$rowNumber] = [$n[$xGroup[0]][$yGroup[0]], $n[$xGroup[0]][$yGroup[1]], $n[$xGroup[0]][$yGroup[2]], $n[$xGroup[1]][$yGroup[0]], $n[$xGroup[1]][$yGroup[1]], $n[$xGroup[1]][$yGroup[2]], $n[$xGroup[2]][$yGroup[0]], $n[$xGroup[2]][$yGroup[1]], $n[$xGroup[2]][$yGroup[2]]];
-            $rowNumber++;
+            $arrays[] = [
+                $n[$xGroup[0]][$yGroup[0]], $n[$xGroup[0]][$yGroup[1]], $n[$xGroup[0]][$yGroup[2]],
+                $n[$xGroup[1]][$yGroup[0]], $n[$xGroup[1]][$yGroup[1]], $n[$xGroup[1]][$yGroup[2]],
+                $n[$xGroup[2]][$yGroup[0]], $n[$xGroup[2]][$yGroup[1]], $n[$xGroup[2]][$yGroup[2]]
+            ];
         }
     }
 
+    //add horizontal and vertical lanes
     for ($i = 0; $i < 9; $i++) {
         $arrays[] = [$n[0][$i], $n[1][$i], $n[2][$i], $n[3][$i], $n[4][$i], $n[5][$i], $n[6][$i], $n[7][$i], $n[8][$i]];
         $arrays[] = $n[$i];
     }
 
-    foreach ($arrays as $array) {
-        if (isValid($array) === false) {
-            $valid = 'false';
-        };
+    if (count(array_filter($arrays, function ($array) {
+        return $array !== array_unique($array);
+    })) > 0) {
+        $valid = 'false';
     }
 
     echo("$valid<br />");
-}
-
-function isValid($array) {
-    return count(array_unique($array)) === 9 && array_sum($array) === 45;
 }
